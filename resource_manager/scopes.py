2d620d40cef394f351f74cf5ac5ce45cb6be63fe
Max <maxs987@gmail.com>
2018-05-04 15:55:26 +0300

    avoiding some deadlocks

--- resource_manager/scopes.py
+++ resource_manager/scopes.py
@@ -1,5 +1,5 @@
 from collections import OrderedDict
-from threading import RLock
+from threading import Lock
 
 from .renderer import Renderer
 from .structures import Structure
@@ -9,31 +9,34 @@ class GlobalScope:
     def __init__(self):
         self._defined_resources = OrderedDict()
         self._undefined_resources = OrderedDict()
-        self._global_lock = RLock()
+        self._local_locks = {}
 
     def overwrite(self, scope):
-        if self._defined_resources or scope._defined_resources:
+        if self._defined_resources and scope._undefined_resources:
             raise RuntimeError("The resource manager's scope already contains rendered resources. "
                                "Overwriting them may lead to undefined behaviour.")
         for name, value in scope._undefined_resources.items():
             self._undefined_resources[name] = value
+            if name not in self._local_locks:
+                self._local_locks[name] = Lock()
 
     def set_node(self, name: str, value: Structure):
         if name in self._undefined_resources:
             raise SyntaxError('Duplicate definition of resource "%s" in %s' % (name, value.source()))
         self._undefined_resources[name] = value
+        self._local_locks[name] = Lock()
 
     def set_resource(self, name: str, value):
-        assert name not in self._defined_resources
+        assert name not in self._defined_resources and name in self._undefined_resources
         self._defined_resources[name] = value
 
     def get_resource(self, name: str):
-        with self._global_lock:
+        if name not in self._undefined_resources:
+            raise AttributeError('Resource "{}" is not defined'.format(name))
+
+        with self._local_locks[name]:
             if name in self._defined_resources:
                 return self._defined_resources[name]
-
-            if name not in self._undefined_resources:
-                raise AttributeError('Resource "{}" is not defined'.format(name))
 
             node = self._undefined_resources[name]
             resource = Renderer.render(node, self)
@@ -51,7 +54,7 @@ class LocalScope:
         self._defined_resources[name] = value
 
     def get_resource(self, name: str):
-        if name in self._defined_resources:
+        try:
             return self._defined_resources[name]
-
-        return self._upper.get_resource(name)
+        except KeyError:
+            return self._upper.get_resource(name)
