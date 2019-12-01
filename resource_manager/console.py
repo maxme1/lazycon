9a8a5b6aed18d62c2e5ae78fb91cbd9bb0eeaaf4
Max <maxs987@gmail.com>
2019-12-01 22:28:48 +0300

    uploaded to pypi

--- resource_manager/console.py
+++ resource_manager/console.py
@@ -16,9 +16,10 @@ def render_config_resource():
 
 
 def build_config():
-    parser = argparse.ArgumentParser(description='Expand all config imports in `source` to create a single config '
-                                                 'with all resources and put it to `target`.')
-    parser.add_argument('source')
-    parser.add_argument('target')
+    parser = argparse.ArgumentParser(
+        description='Read the `input` config and flatten all its imports in order to obtain '
+                    'an `output` config without dependencies.')
+    parser.add_argument('input', help='Path to the input config file.')
+    parser.add_argument('output', help='Path to the output config file.')
     args = parser.parse_args()
-    read_config(args.source).save_config(args.target)
+    read_config(args.input).save_config(args.output)
