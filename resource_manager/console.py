d13811d3c722fcb34fab00ee2753dd64fec614cc
Max <maxs987@gmail.com>
2020-03-05 02:11:37 +0300

    added `eval` method. `run-config` can now evaluate expressions

--- resource_manager/console.py
+++ resource_manager/console.py
@@ -6,13 +6,13 @@ from resource_manager import read_config
 
 
 def render_config_resource():
-    parser = argparse.ArgumentParser(description='Run the given `config` using `name` as an entry point.')
+    parser = argparse.ArgumentParser(description='Run the given `config` by evaluating the passed `expression`.')
     parser.add_argument('config', help='Path to a config file.')
-    parser.add_argument('name', help='The entry point.')
+    parser.add_argument('expression', help='The entry point.')
     # ignoring unknown args as they might be used inside the config
     args = parser.parse_known_args()[0]
 
-    value = read_config(args.config).get_resource(args.name)
+    value = read_config(args.config).eval(args.expression)
     if value is not None:
         print(value)
 
